852ea45cc10c491e03193deda2aa2749a1be835c
Roy Espen Olsen <roy-eo@hotmail.com>
2026-03-05 05:49:28 -0600

    Practice

--- Flask_Basics/Basics_Practice/app.py
+++ Flask_Basics/Basics_Practice/app.py
@@ -3,10 +3,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def home():
     return render_template('index.html')
 
-
+@app.route('/contact')
+def contact(): # url_for points to this function
+    return render_template('contact.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
